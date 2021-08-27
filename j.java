dad62f47855ac1c2ea83168df73590d44da1cfb5
iia7mt <a.ghannam18@student.aaup.edu>
2021-08-27 21:04:47 +0300

    I just changed the java file

---
+++ j.java
@@ -0,0 +1,10 @@
+class hello{
+    int a;
+    int h;
+    String s;
+    
+    public static void main(String[] args) {
+        System.out.println("you are browsing my git hub second attempt !");
+
+    }
+}
